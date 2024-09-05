f59163ffeddbaf4245003e3a8ba1aac9713a91fa
Roman Przybylski <romprzy81@gmail.com>
2024-09-05 21:13:19 +0200

    GoogleMapLoader basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div>Vue 3 Gmaps Plugin</div>
-  <GoogleMap />
-  <GoogleMapLoader />
+  <GoogleMapLoader
+    :loader-options="loaderOptions"
+    :map-options="mapOptions"
+  />
 </template>
+
+<script setup lang="ts">
+import { LoaderOptions } from '@googlemaps/js-api-loader'
+
+const loaderOptions: Partial<LoaderOptions> = {
+  version: 'weekly',
+  libraries: ['places'],
+}
+
+const mapOptions = {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 6,
+}
+</script>
--- src/vue3-gmaps-plugin/components/GoogleMapLoader.vue
+++ src/vue3-gmaps-plugin/components/GoogleMapLoader.vue
@@ -1,3 +1,45 @@
 <template>
-  <div class="google-map-loader">GoogleMapLoader</div>
+  <div
+    class="google-map-loader"
+    style="height: 80vh; background: red; position: relative;"
+  >
+    <div
+      ref="mapContainer"
+      class="map-container"
+      style="position: absolute; top: 0; left: 0; bottom: 0; right: 0;"
+    >
+      <slot>{{ errorText }}</slot>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { Loader, LoaderOptions } from '@googlemaps/js-api-loader'
+import { ref } from 'vue'
+import MapOptions = google.maps.MapOptions
+
+interface IGoogleMapLoaderProps {
+  loaderOptions?: Partial<LoaderOptions>
+  mapOptions?: MapOptions
+}
+
+const props = defineProps<IGoogleMapLoaderProps>()
+
+const loader = new Loader(props.loaderOptions as LoaderOptions)
+const errorText = ref<string>('')
+
+const mapContainer = ref()
+let map: google.maps.Map
+
+loader
+  .importLibrary('maps')
+  .then(async ({ Map }) => {
+    errorText.value = ''
+    map = new Map(mapContainer.value, props.mapOptions)
+    console.log('map', map)
+  })
+  .catch((e) => {
+    errorText.value = 'Could not load Google map'
+    console.log(e)
+  })
+</script>
