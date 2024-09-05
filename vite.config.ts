7b7f6d29d6b5f3f6ae2acfc5683665631a8eb343
Roman Przybylski <romprzy81@gmail.com>
2024-09-05 16:25:29 +0200

    config: vite (build)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/vue3-gmaps-plugin/vue3-gmaps-plugin.ts'),
+      name: 'vue3-gmaps-plugin',
+      fileName: 'vue3-gmaps-plugin',
+    },
+    rollupOptions: {
+      input: './src/vue3-gmaps-plugin/vue3-gmaps-plugin.ts',
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
 })
