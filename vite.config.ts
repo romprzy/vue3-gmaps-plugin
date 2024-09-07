4007fa7ee46bb8456a33e4edf06cc7ec06a143ce
Roman Przybylski <romprzy81@gmail.com>
2024-09-07 17:23:18 +0200

    Add types declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,10 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    }),
+  ],
 })
