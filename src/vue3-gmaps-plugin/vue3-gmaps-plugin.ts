f96460dd52f72465d8c66cbcfb42359bedfdfa30
Roman Przybylski <romprzy81@gmail.com>
2024-09-07 17:33:51 +0200

    Refactor plugin export

--- src/vue3-gmaps-plugin/vue3-gmaps-plugin.ts
+++ src/vue3-gmaps-plugin/vue3-gmaps-plugin.ts
@@ -2,10 +2,8 @@ import { App, Plugin } from 'vue'
 import { GoogleMap, GoogleMapLoader } from './components'
 import './style.scss'
 
-const Vue3GmapsPlugin: Plugin = {
-  install(app: App) {
-    app.component('GoogleMap', GoogleMap)
-    app.component('GoogleMapLoader', GoogleMapLoader)
-  },
+const Vue3GmapsPlugin: Plugin = (app: App) => {
+  app.component('GoogleMap', GoogleMap)
+  app.component('GoogleMapLoader', GoogleMapLoader)
 }
 export default Vue3GmapsPlugin
